3a6e4eb2f248299fa6b23cc63b65e4ee32ff527b
Matt Warren <mwarren@harding.edu>
2017-07-10 16:04:51 -0400

    add api request method into player model

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,29 @@
+require 'net/http'
+require 'net/https'
+
 class Player < ApplicationRecord
   belongs_to :team
+
+	# Request (GET )
+	def send_request
+	  uri = URI("https://www.mysportsfeeds.com/api/feed/pull/nba/2017-playoff/active_players.json")
+
+	  # Create client
+	  http = Net::HTTP.new(uri.host, uri.port)
+	  http.use_ssl = true
+	  http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+
+	  # Create Request
+	  req =  Net::HTTP::Get.new(uri)
+	  # Add headers
+	  req.add_field "Authorization", "Basic " + base64_encode(ENV['USER'] + ":" + ENV['PASS'])
+
+	  # Fetch Request
+	  res = http.request(req)
+	  @result = JSON.parse(res)
+	  puts "Response HTTP Status Code: #{res.code}"
+	  puts "Response HTTP Response Body: #{res.body}"
+	rescue StandardError => e
+	  puts "HTTP Request failed (#{e.message})"
+	end
 end
