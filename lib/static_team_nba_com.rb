3b417881f8b29dbc8e70e9489cbcc9860523a1e0
Matt Warren <mwarren@harding.edu>
2017-07-15 11:28:43 -0400

    recopy static_team_nba_com.rb to be included in lib

--- lib/static_team_nba_com.rb
+++ lib/static_team_nba_com.rb
@@ -1,42 +1,50 @@
-
 require 'open-uri'
 require 'selenium-webdriver'
 
-# html_data = open('http://nba.com/players')
-# nokogiri_object = Nokogiri::HTML(html_data)
-# # tagcloud_elements = nokogiri_object.xpath("//a/@href")
-# # tagcloud_elements = nokogiri_object.xpath("//a[contains(@href, '/players/')]/@href")
-# tagcloud_elements = nokogiri_object.xpath("//a/@href")
+module StaticTeamNbaCom
+	def self.navigate_to_page abbr
+		Selenium::WebDriver::Chrome.driver_path="/Users/mattwarren/dev/chrome_web_driver/chromedriver"
+		driver = Selenium::WebDriver.for :chrome
+		get_data_from(driver, abbr)	
+	end
 
-# tagcloud_elements.each do |tagcloud_element|
-#   puts tagcloud_element.text
-# end
+	def self.get_data_from driver, abbr
+		driver.navigate.to 'http://stats.nba.com/teams'
+		static_teams_links = driver.find_elements(:css, 'a.stats-team-list__link')
+		static_teams_img_abbr = driver.find_elements(:css, 'img.stats-team-list__team-logo.team-img')
+		strip_data([static_teams_links, static_teams_img_abbr], abbr)
+	end
 
-Selenium::WebDriver::Chrome.driver_path="/Users/mattwarren/dev/chrome_web_driver/chromedriver"
-driver = Selenium::WebDriver.for :chrome
-driver.navigate.to 'http://nba.com/players'
-static_player_nba_com = driver.find_elements(:css, 'a.row.playerList')
-player_links = []
-elements = []
-players = []
-static_player_nba_com.each do |data|
-	player_links.push(data.attribute("href"))
-	# ids.push(data.attribute("href").gsub(/[^\d]/, '').to_i) <= this was a regex that grabbed the id in the url
+	def self.strip_data arr, abbr
+		team_nba_com_ids = []
+		team_abbreviations = []
+		arr[0].each do |data|
+			team_nba_com_ids.push(data.attribute("href").gsub(/[^\d]/, '').to_i)
+		end
+		arr[1].each do |data|
+			team_abbreviations.push(data.attribute("abbr"))
+		end
+		create_hash_from_arrays([team_abbreviations, team_nba_com_ids], abbr)
+
+	end 
+
+	def self.create_hash_from_arrays arr, abbr
+		static_teams_nba_com_hash = Hash[arr[0].map {|x| [x, 1]}]
+		static_teams_nba_com_hash.each_with_index do |(key, value), i|
+  			static_teams_nba_com_hash[key] = arr[1][i]
+  		end
+  		sort_hash(static_teams_nba_com_hash, abbr)
+
+  	end
+
+  	def self.sort_hash h, abbr
+  		sorted_nba_com_teams_hash = Hash[ h.sort_by { |key, value| key } ]  		
+  		search(abbr, sorted_nba_com_teams_hash)
+  	end
+
+  	def self.search abbr, h
+  		return h[abbr]
+  	end
 end
 
 
-
-player_links.each do |link|
-	elements.push(link.gsub(/http:\/\/www.nba.com\/players\//, '').split("/"))
-end
-
-elements.each do |e|
-  player_hash = {}
-  player_hash[:first_name] = e[0]
-  player_hash[:last_name] = e[1]
-  player_hash[:nba_com] = e[2].to_i
-  players.push(player_hash)
-end
-
-p players
-p static_player_nba_com.length
