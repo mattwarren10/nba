e7ed0b45ecb0aa2f1b5e02c1ccb8077b0e082c0d
Matt Warren <mwarren@harding.edu>
2017-07-15 13:58:56 -0400

    update static_teams_attributes module to include other modules

--- lib/static_team_attributes.rb
+++ lib/static_team_attributes.rb
@@ -2,11 +2,11 @@ require_relative 'static_team_nba_com'
 require_relative 'static_team_wikipedia'
 
 module StaticTeamAttributes
-	extend StaticTeamNbaCom
-	extend StaticTeamWikipedia
+	include StaticTeamNbaCom
+	include StaticTeamWikipedia
 
 	@@ids = StaticTeamNbaCom.navigate_to_page
-	@@teams = StaticTeamWikipedia.retrieve_from_wikipedia
+	@@teams = StaticTeamWikipedia.retrieve_from_wikipedia # @@teams is updated to have ids when get_teams is called
 
 	def self.add_nba_com_to_teams
 		@@teams.each do |team|
@@ -21,6 +21,6 @@ module StaticTeamAttributes
 	end
 end
 
-p StaticTeamAttributes.get_teams
+
 
 # p teams[0][:city]
