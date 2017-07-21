f02f4aab222698df6ee0bdbc93755ff740b53b83
Matt Warren <mwarren@harding.edu>
2017-07-21 16:33:25 -0400

    create method isolate in CityNameAbbr module

--- lib/player.rb
+++ lib/player.rb
@@ -48,50 +48,28 @@ module Player
 
 	module Profile		
 		def self.retrieve
-			player_nba_com_ids = NbaCom.retrieve
-			player_nba_com_ids.each do |id|
-				profile_src = open("http://stats.nba.com/player/#!/#{id}/")
-				profile_dest = "lib/assets/#{id}.html"
-				IO.copy_stream(profile_src, profile_dest)
-				if File.file? profile_src
-					puts "Player #{id}" has been saved
-				else
-					puts "Failed to store player #{id}"
-					break
-				end
-
-			end
-			# xpath: "//span[@class='player-stats__stat-value']"
-			# players = []
 			# player_nba_com_ids = NbaCom.retrieve
-			# count = 0			
 			# player_nba_com_ids.each do |id|
-			# 	credentials = []				
-			# 	selenium_elements = ChromeSelenium.send('span.player-stats__stat-value', "http://stats.nba.com/player/#!/#{id}/")
-			# 	selenium_elements.first.shift(4)
-			# 	selenium_elements.first.each do |data|
-			# 		credentials.push(data.text)
+			# 	profile_src = open("http://stats.nba.com/player/#!/#{id}/")
+			# 	profile_dest = "lib/assets/#{id}.html"
+			# 	IO.copy_stream(profile_src, profile_dest)
+			# 	if File.file? profile_src
+			# 		puts "Player #{id} has been saved"
+			# 	else
+			# 		puts "Failed to store player #{id}"
+			# 		break
 			# 	end
-			# 	puts "Quitting driver..."
-			# 	ChromeSelenium.quit(selenium_elements.last)				
-			# 	players.push(credentials)
-			# 	count += 1
-			# 	puts "Credentials for id #{id}:"
-			# 	puts "#{credentials}"
-			# 	puts "==> #{player_nba_com_ids.length - count} more players to go."				
-			# end
-			# players
 
-			############# GETTING ONE PLAYER'S CREDENTIALS ####################			
-			# credentials = []
-			# selenium_elements = ChromeSelenium.send("img.player-img", "http://stats.nba.com/player/#!/202331")
-			# # selenium_elements.first.shift(4)
-			# selenium_elements.first.each do |data|
-			# 	credentials.push(data.attribute("team-id"))
 			# end
-			# ChromeSelenium.quit(selenium_elements.last)
-			# credentials
-
+			
+			profile_src = open("http://stats.nba.com/player/#!/202331/")
+			profile_dest = "lib/assets/202331.html"
+			IO.copy_stream(profile_src, profile_dest)
+			if File.file? profile_src
+				puts "Player 202331 has been saved"
+			else
+				puts "Failed to store player #{id}"				
+			end
 		end
 	end
 
--- lib/team.rb
+++ lib/team.rb
@@ -13,45 +13,34 @@ module Team
 			noko_string = retrieve
 			arr = noko_string.split("\n")
 		    4.times { arr.shift }
-		    arr.delete("")		
+		    arr.delete("")	
+		    arr	
+		end
+
+		def self.isolate
+			arr = separate
+			team_names = []
+		    team_cities = []
+		    team_abbr = []
+		    arr.each_with_index do |str, i|
+		      if i.odd?
+		        team_names.push(str.split.last)
+		        team_cities.push(str[0..str.rindex(' ')].rstrip)
+		      end
+		    end
+		    arr.each_with_index do |abbr, i|
+		      if i.even?
+		        team_abbr.push(abbr)
+		      end
+		    end
+		    team_cities, team_names, team_abbr
 		end
 				
 	end
 
-	module Abbr		
-		def self.retrieve
-			selenium_elements = ChromeSelenium.send('img.stats-team-list__team-logo.team-img', URL)
-			team_abbreviations = []
-			selenium_elements.first.each do |data|
-				team_abbreviations.push(data.attribute("abbr"))
-			end
-			ChromeSelenium.quit(selenium_elements.last)
-			team_abbreviations			
-		end		
-	end
-
-	module NbaCom			
-		def self.retrieve
-			selenium_elements = ChromeSelenium.send('a.stats-team-list__link', URL)			
-			team_nba_com_ids = []			
-			selenium_elements.first.each do |data|
-				team_nba_com_ids.push(data.attribute("href").gsub(/[^\d]/, '').to_i)
-			end
-			ChromeSelenium.quit(selenium_elements.last)			
-			team_nba_com_ids
-		end
-	end
-
 	module Attr
 		def self.get #invoke this method to return an array of hashes containing team city, name, abbreviation, and nba_com
-			teams = CityAndName.separate
-			abbr = Abbr.retrieve
-			ids = NbaCom.retrieve							  		
-	  		teams.each_with_index do |team_hash, i|
-	  			team_hash[:abbreviation] = abbr[i]
-	  			team_hash[:nba_com] = ids[i]
-	  		end
-			teams.sort_by { |team_hash| team_hash[:city] }   		
+			  		
 	  	end		  	
 	end	
 end
