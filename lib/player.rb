e7a5f10fd4eeee2b3d55258c34ac7885c5b6e8e1
Matt Warren <mwarren@harding.edu>
2017-07-22 14:53:10 -0400

    parse nokogiri array in Credentials module in Player

--- lib/player.rb
+++ lib/player.rb
@@ -1,86 +1,30 @@
 
 module Player
-	include ChromeSelenium
 
-	module Info
-		def self.retrieve			
-			url = 'http://stats.nba.com/players/list#!?Historic=Y'
-			class_name = 'a.players-list__name'
-			selenium_elements = ChromeSelenium.send(class_name, url)		
-		end
-	end
-
-	module Name
+	module Credentials
 		def self.retrieve
-			selenium_elements = Info::retrieve
-			names = []
-			selenium_elements.first.each do |data|
-				names.push(data.text)
-			end
-			ChromeSelenium.quit(selenium_elements.last)
-			names
+			rosters = Team::Roster.retrieve
 		end
 
-		def self.separate
-			full_names = retrieve 
-			names = []
-			full_names.each do |name|
-				player = {}
-			    player[:last_name] = name.split(", ").first
-			    player[:first_name] = name.split(", ").last
-			    names.push(player)
+		def self.parse
+			noko_arr = retrieve
+			teams = []
+			noko_arr.each do |t|
+				team = []
+				t.each do |player|					
+					team.push(player.text)
+				end
+				teams.push(team)
 			end
-			names
+			teams
 		end
-	end
 
-	module NbaCom
-		def self.retrieve
-			selenium_elements = Info::retrieve
-			player_nba_com_ids = []
-			selenium_elements.first.each do |data|
-				player_nba_com_ids.push(data.attribute("href").gsub(/[^\d]/, '').to_i)
-			end
-			ChromeSelenium.quit(selenium_elements.last)
-			player_nba_com_ids
-		end
-	end
 
-	module Profile		
-		def self.retrieve
-			# player_nba_com_ids = NbaCom.retrieve
-			# player_nba_com_ids.each do |id|
-			# 	profile_src = open("http://stats.nba.com/player/#!/#{id}/")
-			# 	profile_dest = "lib/assets/#{id}.html"
-			# 	IO.copy_stream(profile_src, profile_dest)
-			# 	if File.file? profile_src
-			# 		puts "Player #{id} has been saved"
-			# 	else
-			# 		puts "Failed to store player #{id}"
-			# 		break
-			# 	end
-
-			# end
-			
-			profile_src = open("http://stats.nba.com/player/#!/202331/")
-			profile_dest = "lib/assets/202331.html"
-			IO.copy_stream(profile_src, profile_dest)
-			if File.file? profile_src
-				puts "Player 202331 has been saved"
-			else
-				puts "Failed to store player #{id}"				
-			end
-		end
 	end
 
 	module Attr
 		def self.get
-			players = Name.separate
-			ids = NbaCom.retrieve
-			players.each_with_index do |player_hash, i|
-				player_hash[:nba_com] = ids[i]
-			end
-			players
+			
 		end
 	end
 end
