a7b50b9a8d1320a0916c06dc6d37a1133763cb41
Matt Warren <mwarren@harding.edu>
2017-07-28 16:45:13 -0400

    add tests to static_player_stats to validate each attribute

--- app/models/static_player_stat.rb
+++ app/models/static_player_stat.rb
@@ -1,3 +1,17 @@
 class StaticPlayerStat < ApplicationRecord
   belongs_to :static_player
+
+  validates_presence_of :season,
+  						:team,
+  						:games_played,
+  						:games_started,
+  						:minutes_per_game,
+  						:field_goal_percent,
+  						:three_point_percent,
+  						:free_throw_percent,
+  						:rebounds_per_game,
+  						:assists_per_game,
+  						:steals_per_game,
+  						:blocks_per_game,
+  						:points_per_game
 end
--- spec/models/static_player_stat_spec.rb
+++ spec/models/static_player_stat_spec.rb
@@ -6,32 +6,60 @@ RSpec.describe StaticPlayerStat, type: :model do
   end
   describe 'creation' do
   	it 'cannot be created without a season' do
+  		@static_player_stat.season = nil
+  		expect(@static_player_stat).to_not be_valid
   	end
-  	xit 'cannot be created without a team' do
+  	it 'cannot be created without a team' do
+  		@static_player_stat.team = nil
+  		expect(@static_player_stat).to_not be_valid
   	end
-  	xit 'cannot be created without games_played' do
+  	it 'cannot be created without games_played' do
+  		@static_player_stat.games_played = nil
+  		expect(@static_player_stat).to_not be_valid
   	end
-  	xit 'cannot be created without games_started' do
+  	it 'cannot be created without games_started' do
+  		@static_player_stat.games_started = nil
+  		expect(@static_player_stat).to_not be_valid
   	end
-  	xit 'cannot be created without minutes_per_game' do
+  	it 'cannot be created without minutes_per_game' do
+  		@static_player_stat.minutes_per_game = nil
+  		expect(@static_player_stat).to_not be_valid
   	end
-  	xit 'cannot be created without field_goal_percent' do
+  	it 'cannot be created without field_goal_percent' do
+  		@static_player_stat.field_goal_percent = nil
+  		expect(@static_player_stat).to_not be_valid
   	end
-  	xit 'cannot be created without three_point_percent' do
+  	it 'cannot be created without three_point_percent' do
+  		@static_player_stat.three_point_percent = nil
+  		expect(@static_player_stat).to_not be_valid
   	end
-  	xit 'cannot be created without free_throw_percent' do
+  	it 'cannot be created without free_throw_percent' do
+  		@static_player_stat.free_throw_percent = nil
+  		expect(@static_player_stat).to_not be_valid
   	end
-  	xit 'cannot be created without rebounds_per_game' do
+  	it 'cannot be created without rebounds_per_game' do
+  		@static_player_stat.rebounds_per_game = nil
+  		expect(@static_player_stat).to_not be_valid
   	end
-  	xit 'cannot be created without assists_per_game' do
+  	it 'cannot be created without assists_per_game' do
+  		@static_player_stat.assists_per_game = nil
+  		expect(@static_player_stat).to_not be_valid
   	end
-  	xit 'cannot be created without steals_per_game' do
+  	it 'cannot be created without steals_per_game' do
+  		@static_player_stat.steals_per_game = nil
+  		expect(@static_player_stat).to_not be_valid
   	end
-  	xit 'cannot be created without blocks_per_game' do
+  	it 'cannot be created without blocks_per_game' do
+  		@static_player_stat.blocks_per_game = nil
+  		expect(@static_player_stat).to_not be_valid
   	end
-  	xit 'cannot be created without points_per_game' do
+  	it 'cannot be created without points_per_game' do
+  		@static_player_stat.points_per_game = nil
+  		expect(@static_player_stat).to_not be_valid
   	end
-  	xit 'cannot be created without a static_player id' do
+  	it 'cannot be created without a static_player id' do
+  		@static_player_stat.static_player_id = nil
+  		expect(@static_player_stat).to_not be_valid
   	end
   end
 end
