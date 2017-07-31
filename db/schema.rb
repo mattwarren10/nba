c525bb30622fe6d9bb9a622f36457a08843d8891
Matt Warren <mwarren@harding.edu>
2017-07-31 14:31:18 -0400

    generate league model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170731181613) do
+ActiveRecord::Schema.define(version: 20170731183051) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "leagues", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "static_player_stats", force: :cascade do |t|
     t.bigint "static_player_id"
