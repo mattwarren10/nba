3205200828e391a04ceedce558df1ff18b09bafd
Matt Warren <mwarren@harding.edu>
2017-08-01 12:15:16 -0400

    add user factory girls

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,17 @@
 FactoryGirl.define do
-  factory :user do
-    
+  factory :admin_user, class: "AdminUser" do
+  	last_name "Warren"
+  	first_name "Matt"
+    email "mwarren@harding.edu"
+    password "weeboo"
+    password_confirmation "weeboo"    
+  end
+
+  factory :basic_user, class: "BasicUser" do
+  	last_name "Mardanzai"
+  	first_name "Louis"
+  	email "malwarr@indiana.edu"
+  	password "tarbooz"
+  	password_confirmation "tarbooz"
   end
 end
