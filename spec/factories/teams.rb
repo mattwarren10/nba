49a5c327ab8a931d0f61e4ec91ff9dc81d09202d
Matt Warren <mwarren@harding.edu>
2017-08-01 13:55:50 -0400

    add authentic and fantasy team factory girls

--- spec/factories/teams.rb
+++ spec/factories/teams.rb
@@ -2,13 +2,43 @@ FactoryGirl.define do
 	factory :team, class: "Team" do
 		city "Indiana"
 		name "Pacers"
-		abbreviation "IND"		
+		abbreviation "IND"
+		category 0	
 	end
 
 	factory :team_duplicate, class: "Team" do
 		city "Indiana"
 		name "Pacers"
-		abbreviation "IND"		
+		abbreviation "IND"
+		category 0	
+	end
+
+	factory :team_fantasy_one, class: "Team" do
+		city "New York"
+		name "Legends"
+		abbreviation "NYL"
+		category 1
+	end
+
+	factory :team_fantasy_two, class: "Team" do
+		city "Los Angeles"
+		name "Drama"
+		abbreviation "LAD"
+		category 1
+	end
+
+	factory :team_fantasy_three, class: "Team" do
+		city "Tokyo"
+		name "Samurai"
+		abbreviation "TOK"
+		category 1
+	end
+
+	factory :team_fantasy_four, class: "Team" do
+		city "Kabul"
+		name "Emeralds"
+		abbreviation "KAB"
+		category 1
 	end
 
 end
