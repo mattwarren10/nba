0f51b70e943ce604b49cc91723388ff364cd0308
Matt Warren <mwarren@harding.edu>
2017-08-01 12:44:04 -0400

    add league factory girls. add abbreviation attr to League model

--- spec/factories/leagues.rb
+++ spec/factories/leagues.rb
@@ -1,5 +1,23 @@
 FactoryGirl.define do
-  factory :league do
-    
+  admin_user = FactoryGirl.create(:admin_user)
+  factory :league_nba do
+    name "National Basketball Association"
+    commissioner "Adam Silver"
+    headquarters "New York, NY"
+    inaugural_season "1946-47"
+    number_of_teams 30
+    abbreviation "NBA"
+    users {[admin_user]}
+  end
+
+  basic_user = FactoryGirl.create(:basic_user)
+  factory :league_fantasy do
+  	name "World Basketball League"
+  	commissioner basic_user.username
+  	headquarters "Indianapolis, IN"
+  	inaugural_season "2017-18"
+  	number_of_teams 30
+  	abbreviation "WBL"
+  	users { [ admin_user, basic_user ] }
   end
 end
