5a63e130fb22a0d463359ff8d300ee23129c12e9
Matt Warren <mwarren@harding.edu>
2017-08-02 14:42:13 -0400

    generate Leagues controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
+
   devise_for :users
   root :to => 'static#home'
 
   # scope league and nest teams and players inside
+  resources :leagues
   resources :players, only: [:index, :show]
   resources :teams, only: [:index, :show]
 
--- spec/controllers/leagues_controller_spec.rb
+++ spec/controllers/leagues_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe LeaguesController, type: :controller do
-	
+	describe 'accessing Leagues' do
+		it 'requires login' do
+			get :index
+			expect(response).to redirect_to new_user_session
+		end
+	end
 end
