bbb109708e576baab11cdf6b6ab4d2af9cbc8d13
Matt Warren <mwarren@harding.edu>
2017-08-03 12:14:59 -0400

    add presence validations for retired players for stat spec

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -4,4 +4,20 @@ class Stat < ApplicationRecord
 
   belongs_to :league_player, optional: true
   belongs_to :league_team
+
+  validates_presence_of :season,
+  						:team,
+  						:games_played,
+  						# :games_started,
+  						:minutes_per_game,
+  						:field_goal_percent,
+  						# :three_point_percent,
+  						:free_throw_percent,
+  						:rebounds_per_game,
+  						:assists_per_game,
+  						:blocks_per_game,
+  						:points_per_game
+
+ # add validation for presence of games_started, three_point_percent,
+ # steals and blocks only if the player's status is active
 end
--- spec/models/stat_spec.rb
+++ spec/models/stat_spec.rb
@@ -5,8 +5,59 @@ RSpec.describe Stat, type: :model do
 		before do								
 			@stat = create(:stat)
 		end
-		it 'cannot be created without games played' do
+		it 'cannot be created without a season' do
+			@stat.season = nil
+			expect(@stat).to_not be_valid
+		end
+		it 'cannot be created without a team' do
+			@stat.team = nil
+			expect(@stat).to_not be_valid
+		end
+		it 'cannot be created without games_played' do
 			@stat.games_played = nil
+			expect(@stat).to_not be_valid
+		end
+		# games started wasn't recorded until 1981-82 season
+		# it 'cannot be created without games_started' do
+		# 	@stat.games_started = nil
+		# 	expect(@stat).to_not be_valid
+		# end
+		it 'cannot be created without minutes_per_game' do
+			@stat.minutes_per_game = nil
+			expect(@stat).to_not be_valid
+		end
+		it 'cannot be created without field_goal_percent' do
+			@stat.field_goal_percent = nil
+			expect(@stat).to_not be_valid
+		end
+		# three pointers weren't recorded until 1979-80 season
+		# it 'cannot be created without three_point_percent' do
+		# 	@stat.three_point_percent = nil
+		# 	expect(@stat).to_not be_valid
+		# end
+		it 'cannot be created without free_throw_percent' do
+			@stat.free_throw_percent = nil
+			expect(@stat).to_not be_valid
+		end
+		it 'cannot be created without rebounds_per_game' do
+			@stat.rebounds_per_game = nil
+			expect(@stat).to_not be_valid
+		end
+		it 'cannot be created without assists_per_game' do
+			@stat.assists_per_game = nil
+			expect(@stat).to_not be_valid
+		end
+		# steals weren't recorded until 1973-74 season
+		# it 'cannot be created without steals_per_game' do
+		# 	@stat.steals_per_game = nil
+		# 	expect(@stat).to_not be_valid
+		# end
+		it 'cannot be created without blocks_per_game' do
+			@stat.blocks_per_game = nil
+			expect(@stat).to_not be_valid
+		end
+		it 'cannot be created without points_per_game' do
+			@stat.points_per_game = nil
 			expect(@stat).to_not be_valid
 		end
 	end
