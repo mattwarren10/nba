f1b1543cf2bdc259d051fdda0254bbde9fbbab5a
Matt Warren <mwarren@harding.edu>
2017-08-03 10:43:10 -0400

    add correct syntax for association player to league players factory

--- spec/factories/league_players.rb
+++ spec/factories/league_players.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :league_player, class: "LeaguePlayer" do
     association :league, factory: :league_fantasy_one
-    player_one
+    association :player, factory: :player_one
   end
 end
