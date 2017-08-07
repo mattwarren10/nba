e0d04b6654b2892091e431232009b89243a0fdf5
Matt Warren <mwarren@harding.edu>
2017-08-07 19:52:27 -0400

    add tests to league_team_spec

--- app/models/league_team.rb
+++ app/models/league_team.rb
@@ -4,4 +4,6 @@ class LeagueTeam < ApplicationRecord
 	belongs_to :user, optional: true
 	has_many :league_players
 	has_many :stats
+
+	validates :league_id, uniqueness: { scope: :team_id }
 end
--- spec/models/league_team_spec.rb
+++ spec/models/league_team_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe LeagueTeam, type: :model do
-
+  before do
+  	@league_team_one = create(:league_team_one)
+  end
+  describe 'creation' do
+  	it 'can be created' do
+  	  expect(@league_team_one).to be_valid
+  	end
+  	it 'cannot be created without a league_id' do
+  		@league_team_one.league_id = nil
+  		expect(@league_team_one).to_not be_valid
+  	end
+  	it 'cannot be created without a team_id' do
+  		@league_team_one.team_id = nil
+  		expect(@league_team_one).to_not be_valid
+  	end
+  	it 'cannot be duplicated' do
+  		league_team_two = create(:league_team_two)
+  		league_team_two.league_id = @league_team_one.league_id
+  		league_team_two.team_id = @league_team_one.team_id
+  		expect(league_team_two).to_not be_valid
+  	end
+  end
 end
