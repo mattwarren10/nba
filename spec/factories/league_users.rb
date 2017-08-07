7b3c423880275c06e21c7fbfaaf30a1ce8d4a96b
Matt Warren <mwarren@harding.edu>
2017-08-07 11:16:06 -0400

    add factories for league_users

--- spec/factories/league_users.rb
+++ spec/factories/league_users.rb
@@ -1,5 +1,14 @@
 FactoryGirl.define do
-  factory :league_user do
-    
+  factory :league_admin_user_one do
+  	association :league, factory: :league_nba
+  	association :user, factory: :admin_user
+  end
+  factory :league_basic_user_one do
+    association :league, factory: :league_fantasy_one
+    association :user, factory: :basic_user_one
+  end
+  factory :league_basic_user_two do
+  	association :league, factory: :league_fantasy_two
+  	association :user, factory: :basic_user_two
   end
 end
