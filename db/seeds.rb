b5106172c6dd20457f27033d03624487f88da16d
Matt Warren <mwarren@harding.edu>
2017-08-07 19:32:07 -0400

    adjust seeds to have leagues created after users have been added to it

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-AdminUser.create(last_name: "Warren", first_name: "Matt", username: "mattwarren", email: "mwarren@harding.edu", password: "weeboo", password_confirmation: "weeboo")
+au = AdminUser.create(last_name: "Warren", first_name: "Matt", username: "mattwarren", email: "mwarren@harding.edu", password: "weeboo", password_confirmation: "weeboo")
 
-BasicUser.create(last_name: "Mardanzai", first_name: "Louis", username: "louismardanzai", email: "louismardanzai@indiana.com", password: "pacers", password_confirmation: "pacers")
-BasicUser.create(last_name: "Mardanzai", first_name: "Mosa", username: "mosamardanzai", email: "mosamardanzai@indiana.edu", password: "tarbooz", password_confirmation: "tarbooz")
+bu1 = BasicUser.create(last_name: "Mardanzai", first_name: "Louis", username: "louismardanzai", email: "louismardanzai@indiana.com", password: "pacers", password_confirmation: "pacers")
+bu2 = BasicUser.create(last_name: "Mardanzai", first_name: "Mosa", username: "mosamardanzai", email: "mosamardanzai@indiana.edu", password: "tarbooz", password_confirmation: "tarbooz")
 
-League.create(name: "National Basketball Association", commissioner: "Adam Silver", headquarters: "New York, NY", inaugural_season: "1946-1947", abbreviation: "NBA")
-League.create(name: "World Basketball League", commissioner: "mosamardanzai", headquarters: "Indianapolis, IN", inaugural_season: "2017-18", abbreviation: "WBL")
-League.create(name: "Afghanistan National League", commissioner: "louismardanzai", headquarters: "Kabul, AF", inaugural_season: "2008-09", abbreviation: "ANL")
+nba = League.new(name: "National Basketball Association", commissioner: "Adam Silver", headquarters: "New York, NY", inaugural_season: "1946-1947", abbreviation: "NBA")
+nba.users.push(au)
+nba.save
+
+league_fantasy_one = League.new(name: "World Basketball League", commissioner: "mosamardanzai", headquarters: "Indianapolis, IN", inaugural_season: "2017-18", abbreviation: "WBL")
+league_fantasy_one.users.push(bu1)
+league_fantasy_one.save
+
+league_fantasy_two = League.new(name: "Afghanistan National League", commissioner: "louismardanzai", headquarters: "Kabul, AF", inaugural_season: "2008-09", abbreviation: "ANL")
+league_fantasy_two.users.push(bu2)
+league_fantasy_two.save
 
 Player.create(last_name: "Warren", first_name: "Matt", jersey_number: "33", height: 200, weight: 85, birth_date: DateTime.now	- 25.years, before_nba: "Indiana", is_rookie: true, position: "PG", which_pick: "2010 / Round: 1 / Pick: 1st overall", years_pro: 0, status: 0, from_city: "Bloomington, Indiana", wiki_link: "Matt_Warren", image_link: "//upload.wikimedia.org/Matt_Warren") 
 
