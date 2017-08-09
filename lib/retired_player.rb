a8e7b92f515d3ff22014f8931886dda98935c371
Matt Warren <mwarren@harding.edu>
2017-08-09 13:39:39 -0400

    add modify method to RetiredPlayer::Credentials

--- lib/retired_player.rb
+++ lib/retired_player.rb
@@ -51,17 +51,16 @@ module RetiredPlayer
 			links.each do |link|
 				url = "vendor/retired_player_wiki/#{link}.html"
 				vcard = "//table[@class='infobox vcard']"
-				text = ""
 				chain_vcard = "|" + vcard			
 				image_src = vcard + "/tr/td/a/img/@src"
-				birth_date_and_city = chain_vcard + "//tr[contains(., 'Born')]/td" + text
-				height = chain_vcard + "/tr[contains(., 'Listed height')]/td" + text
-				weight = chain_vcard + "/tr[contains(., 'Listed weight')]/td" + text
-				high_school = chain_vcard + "/tr[contains(., 'High school')]/td" + text
-				college = chain_vcard + "/tr[contains(., 'College')]/td" + text
-				position = chain_vcard + "/tr[contains(., 'Position')]/td" + text
-				number = chain_vcard + "/tr[contains(., 'Number')]/td" + text
-				which_pick = chain_vcard + "/tr[contains(., 'NBA draft')]/td" + text
+				birth_date_and_city = chain_vcard + "//tr[contains(., 'Born')]/td"
+				height = chain_vcard + "/tr[contains(., 'Listed height')]/td"
+				weight = chain_vcard + "/tr[contains(., 'Listed weight')]/td"
+				high_school = chain_vcard + "/tr[contains(., 'High school')]/td"
+				college = chain_vcard + "/tr[contains(., 'College')]/td"
+				position = chain_vcard + "/tr[contains(., 'Position')]/td"
+				number = chain_vcard + "/tr[contains(., 'Number')]/td"
+				which_pick = chain_vcard + "/tr[contains(., 'NBA draft')]"
 				season_stats = "|//*[self::h3]/following-sibling::table[@class='wikitable sortable']"
 				xpath = image_src + birth_date_and_city + height + weight + high_school + college + position + number + which_pick + season_stats
 				data = CallNokogiri.xpath(url, xpath).map(&:text).join(";")
@@ -70,6 +69,31 @@ module RetiredPlayer
 				puts "#{player_wiki_count} retired wikis retrieved, currently on #{link}"
 			end
 			player_data
+		end
+
+		def self.modify
+			retired_players_data = retrieve
+			retired_players = []
+			retired_players_data.each do |player_data|
+				player_data.delete ""
+				retired_player = []
+				img_link, birth_date = player_data[0].split(";")
+				birth_date = DateTime.parse birth_date[1..10]
+				retired_player.push(img_link, birth_date)
+				born_city, height, weight, high_school = player_data[1].split(";")
+				height = height[height.index("(")..height.index(")")].gsub!(/\D/, "")
+				weight = weight[weight.index("(")..weight.index(")")].gsub!(/\D/, "")
+				high_school += " " + player_data[2].split(";")[0]
+				college = player_data[2].split(";")[1]
+				college = college[0..college.index("(")].delete("(").rstrip
+				retired_player.push(born_city, height, weight, high_school, college)
+				draft = player_data.grep(/draft/)[0]
+				i = player_data.index(draft) + 1
+				which_pick = player_data[i]
+				retired_player.push(which_pick)
+				retired_players.push(retired_player)
+			end
+			retired_players
 		end
 	end
 
