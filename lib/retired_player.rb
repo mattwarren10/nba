96d764e335d51195b3f9a4a0085b9d46c19283fd
Matt Warren <mwarren@harding.edu>
2017-08-09 09:09:45 -0400

    use regular expression to split names accordingly in RetiredPlayer

--- lib/retired_player.rb
+++ lib/retired_player.rb
@@ -6,21 +6,28 @@ module RetiredPlayer
 		TABLE = "//table[2]/tr/td"
 		SELECT_HOF = "[@bgcolor='#FFFF99']"	# hall of famers
 		SELECT_OTHER = "[@bgcolor='#CCFF99']" # not yet eligible for HOF
-		SPAN = "/span[@class='vcard']/span[@class='fn']"		
-		HALL_OF_FAMERS = TABLE + SELECT_HOF	+ SPAN
-		OTHER = TABLE + SELECT_OTHER + SPAN
+		HALL_OF_FAMERS = TABLE + SELECT_HOF
+		OTHER = TABLE + SELECT_OTHER
 
 		# get names of players
-		def self.retrieve_names				
-			xpath = HALL_OF_FAMERS + '|' + OTHER
+		def self.retrieve_names	
+			span_names = "/span[@class='sortkey']"			
+			xpath = HALL_OF_FAMERS + span_names +'|' + OTHER + span_names
 			data = CallNokogiri.xpath URL, xpath
 		end
 
 		# get wiki_links of players
 		def self.retrieve_wiki_links
 			href = "/a/@href"
-			xpath = HALL_OF_FAMERS + href + '|' + OTHER + href
+			span_wiki_links = "/span[@class='vcard']/span[@class='fn']"
+			xpath = HALL_OF_FAMERS + span_wiki_links + href + '|' + OTHER + span_wiki_links + href
 			data = CallNokogiri.xpath URL, xpath
+		end
+
+		def self.separate_names
+			noko_elements = retrieve_names
+			names = noko_elements.text.split(/(?<!\-)(?<!\')(?<!Mc)(?<!\s)(?=[A-Z])/)
+			
 		end
 
 
